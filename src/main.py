604b1e7fb71a3170d9133d6774a0fcc7c5356c8c
mr_destructive <gormeet711@gmail.com>
2022-11-26 14:52:12 +0530

    feat: get/post option and working req-resp body

--- src/main.py
+++ src/main.py
@@ -38,16 +38,28 @@ class RequestMethods(Static):
         self.styles.height = "auto"
 
 
-class Request(Static):
+class RequestContainer(Static):
     """A APTUI widget."""
+
+    method_choide = "GET"
 
     def get_response(self, url: str) -> str:
         resp = json.loads(requests.get(url).content).__str__()
         return resp
 
+    def get_headers(self) -> dict:
+        headers = self.query(".headers")
+        for header in headers:
+            kv_pair = header
+
+        return {}
+
     def post_request(self, url: str, body: dict, headers: dict) -> str:
-        resp = requests.post(url, data=body, headers=headers).content
-        return json.loads(resp).__str__()
+        # resp = requests.post(url, data=body, headers=headers)
+        resp = requests.request("POST", url, headers=headers, json=body)
+        if resp.status_code not in range(200, 227):
+            return f"{resp.status_code} -> ERROR {resp.json()}"
+        return resp.content.decode("ascii")
 
     def on_input_submitted(self, message: Input.Submitted) -> None:
         url = message.value
@@ -56,30 +68,35 @@ class Request(Static):
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         """Event handler called when a button is pressed."""
-        if event.button.id == "send":
+        button_id = event.button.id
+        if button_id == "send":
             url = self.query(Input).first().value
-            resp = self.get_response(url)
-            self.query_one("#response", Static).update(resp)
+            if self.method_choide == "POST":
+                body = json.loads(self.query_one("#body_inp").value) or {}
+                # headers = self.query(".header").first().value or {}
+                resp = self.post_request(
+                    url, body=body, headers={}
+                )  # , headers=headers)
+                self.query_one("#response", Static).update(resp)
+            else:
+                url = self.query(Input).first().value
+                resp = self.get_response(url)
+                self.query_one("#response", Static).update(resp)
 
-        elif event.button.id == "post":
-            url = self.query(Input).first().value
-            body = self.query_one("#body_inp").value
-            #'''
-            body = {"option": "test 56", "is_correct": True}
-            #'''
-            # headers = self.query("#headers")value
-            headers = {}
-            resp = self.post_request(url, body=body, headers=headers)
-            self.query_one("#response", Static).update(resp)
+        elif button_id == "post":
+            self.method_choide = "POST"
+        elif button_id == "get":
+            self.method_choide = "GET"
 
-            headers = RequestHeader(id="headers")
-            self.query_one("#reqheaders").mount(headers)
-            headers.scroll_visible()
-
-        elif event.button.id == "import":
+        elif button_id == "import":
             curl = pyperclip.paste()
             resp = self.get_response(curl)
             self.query_one("#response", Static).update(resp)
+
+        elif button_id == "add_req":
+            headers = RequestHeader(id="headers")
+            self.query_one("#reqheaders").mount(headers)
+            headers.scroll_visible()
 
     def compose(self) -> ComposeResult:
         """Create child widgets of a API Request."""
@@ -106,7 +123,9 @@ class APTUI(App):
     ]
     content = var("0")
 
-    def add_request_widget(self, req_widget: Request = Request()) -> None:
+    def add_request_widget(
+        self, req_widget: RequestContainer = RequestContainer()
+    ) -> None:
         self.query_one("#aptui").mount(req_widget)
         req_widget.scroll_visible()
 
@@ -114,14 +133,14 @@ class APTUI(App):
         """Called when a button is pressed."""
         button_id = event.button.id
         if button_id == "add_req":
-            self.add_request_widget(Request())
+            self.add_request_widget(RequestContainer())
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
-        yield Button("Add", id="add_req", variant="success")
+        yield Button("Add Request", id="add_req", variant="success")
         yield Header()
         yield Footer()
-        yield Container(Request(), id="aptui")
+        yield Container(RequestContainer(), id="aptui")
 
     def on_mount(self) -> None:
         self.styles.height = "auto"
@@ -136,7 +155,7 @@ class APTUI(App):
 
     def action_add_request(self) -> None:
         """An action to add a request"""
-        self.add_request_widget(Request())
+        self.add_request_widget(RequestContainer())
 
 
 if __name__ == "__main__":
