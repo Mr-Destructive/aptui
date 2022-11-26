ee17dae9dda065a51bb5de2a1f8f64f3432fc934
mr_destructive <gormeet711@gmail.com>
2022-11-26 20:57:55 +0530

    feat: export and import curl requests

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import json
+import curl
+import uncurl
 from typing import List
 import pyperclip
 import requests
@@ -10,13 +12,15 @@ from textual.widgets import Button, Header, Footer, Input, Static
 
 class Response(Static):
     """A widget to display Response"""
+
     def compose(self) -> ComposeResult:
         """Create child widgets of response"""
-        yield Static(id="response_text")
-        yield Static(id="status_code")
+        yield Static("Response", id="response_text")
+        yield Static("Status Code", id="status_code")
 
     def on_mount(self) -> None:
         self.styles.height = "auto"
+
 
 class RequestHeader(Static):
     def compose(self) -> ComposeResult:
@@ -49,7 +53,7 @@ class RequestMethods(Static):
 class RequestContainer(Static):
     """A APTUI widget."""
 
-    method_choide = "GET"
+    method_choice = "GET"
 
     def catch_response(self, resp: requests.Response) -> dict:
         try:
@@ -61,7 +65,10 @@ class RequestContainer(Static):
         resp = requests.get(url)
         if resp.status_code not in range(200, 227):
             return self.catch_response(resp)
-        return {"status_code": str(resp.status_code), "response": resp.content.decode("ascii")}
+        return {
+            "status_code": str(resp.status_code),
+            "response": resp.content.decode("ascii"),
+        }
 
     def get_headers(self) -> dict:
         headers = self.query(".headers")
@@ -75,38 +82,52 @@ class RequestContainer(Static):
         resp = requests.request("POST", url, headers=headers, json=body)
         if resp.status_code not in range(200, 227):
             return self.catch_response(resp)
-        return {"status_code": str(resp.status_code), "response": resp.content.decode("ascii")}
+        return {
+            "status_code": str(resp.status_code),
+            "response": resp.content.decode("ascii"),
+        }
 
     def delete_request(self, url: str) -> dict:
         resp = requests.request("DELETE", url)
         if resp.status_code not in range(200, 227):
             return self.catch_response(resp)
-        return {"status_code": str(resp.status_code), "response": resp.content.decode("ascii")}
+        return {
+            "status_code": str(resp.status_code),
+            "response": resp.content.decode("ascii"),
+        }
 
     def put_request(self, url: str, body: dict, headers: dict) -> dict:
         # resp = requests.post(url, data=body, headers=headers)
         resp = requests.request("PUT", url, headers=headers, json=body)
         if resp.status_code not in range(200, 227):
             return self.catch_response(resp)
-        return {"status_code": str(resp.status_code), "response": resp.content.decode("ascii")}
+        return {
+            "status_code": str(resp.status_code),
+            "response": resp.content.decode("ascii"),
+        }
 
     def patch_request(self, url: str, body: dict, headers: dict) -> dict:
         # resp = requests.post(url, data=body, headers=headers)
         resp = requests.request("PATCH", url, headers=headers, json=body)
         if resp.status_code not in range(200, 227):
             return self.catch_response(resp)
-        return {"status_code": str(resp.status_code), "response": resp.content.decode("ascii")}
+        return {
+            "status_code": str(resp.status_code),
+            "response": resp.content.decode("ascii"),
+        }
 
     def on_input_submitted(self, message: Input.Submitted) -> None:
         url = message.value
-        body = json.loads(self.query_one("#body_inp").value) or {}
-        if self.method_choide == "POST":
+        if self.method_choice == "POST":
+            body = json.loads(self.query_one("#body_inp").value) or {}
             resp = self.post_request(url, body=body, headers={})
-        elif self.method_choide == "PUT":
+        elif self.method_choice == "PUT":
+            body = json.loads(self.query_one("#body_inp").value) or {}
             resp = self.put_request(url, body=body, headers={})
-        elif self.method_choide == "PATCH":
+        elif self.method_choice == "PATCH":
+            body = json.loads(self.query_one("#body_inp").value) or {}
             resp = self.patch_request(url, body=body, headers={})
-        elif self.method_choide == "DELETE":
+        elif self.method_choice == "DELETE":
             resp = self.delete_request(url)
         else:
             resp = self.get_request(url)
@@ -116,27 +137,44 @@ class RequestContainer(Static):
     def on_button_pressed(self, event: Button.Pressed) -> None:
         """Event handler called when a button is pressed."""
         button_id = event.button.id
+        if button_id == "tocurl":
+            url = self.query_one("#url").value
+            body = self.query_one("#body_inp").value or {}
+            method = self.method_choice
+            if not body or button_id in ["DELETE", "GET"]:
+                request = requests.request(method, url)
+            else:
+                body = json.loads(body)
+                request = requests.request(method, url, json=body, headers={})
+            c = curl.parse(request, return_it=True)
+            pyperclip.copy(c)
         if button_id == "send":
             url = self.query(Input).first().value
-            if self.method_choide == "POST":
-                body = json.loads(self.query_one("#body_inp").value) or {}
+            if self.method_choice == "POST":
+                body = self.query_one("#body_inp").value or {}
+                if body:
+                    body = json.loads(body)
                 # headers = self.query(".header").first().value or {}
                 resp = self.post_request(
                     url, body=body, headers={}
                 )  # , headers=headers)
                 self.query_one("#response_text", Static).update(resp["response"])
                 self.query_one("#status_code", Static).update(resp["status_code"])
-            elif self.method_choide == "DELETE":
+            elif self.method_choice == "DELETE":
                 resp = self.delete_request(url=url)
                 self.query_one("#response_text", Static).update(resp["response"])
                 self.query_one("#status_code", Static).update(resp["status_code"])
-            elif self.method_choide == "PUT":
-                body = json.loads(self.query_one("#body_inp").value) or {}
+            elif self.method_choice == "PUT":
+                body = self.query_one("#body_inp").value or {}
+                if body:
+                    body = json.loads(body)
                 resp = self.put_request(url=url, body=body, headers={})
                 self.query_one("#response_text", Static).update(resp["response"])
                 self.query_one("#status_code", Static).update(resp["status_code"])
-            elif self.method_choide == "PATCH":
-                body = json.loads(self.query_one("#body_inp").value) or {}
+            elif self.method_choice == "PATCH":
+                body = self.query_one("#body_inp").value or {}
+                if body:
+                    body = json.loads(body)
                 resp = self.patch_request(url=url, body=body, headers={})
                 self.query_one("#response_text", Static).update(resp["response"])
                 self.query_one("#status_code", Static).update(resp["status_code"])
@@ -146,20 +184,36 @@ class RequestContainer(Static):
                 self.query_one("#response_text", Static).update(resp["response"])
 
         elif button_id == "post":
-            self.method_choide = "POST"
+            self.method_choice = "POST"
         elif button_id == "get":
-            self.method_choide = "GET"
+            self.method_choice = "GET"
         elif button_id == "delete":
-            self.method_choide = "DELETE"
+            self.method_choice = "DELETE"
         elif button_id == "put":
-            self.method_choide = "PUT"
+            self.method_choice = "PUT"
         elif button_id == "patch":
-            self.method_choide = "PATCH"
+            self.method_choice = "PATCH"
 
-        elif button_id == "import":
-            curl = pyperclip.paste()
-            resp = self.get_request(curl)
-            self.query_one("#response", Static).update(resp)
+        elif button_id == "importcurl":
+            curl_request = pyperclip.paste()
+            req_widget = RequestContainer()
+            self.parent.mount(req_widget)
+            new_req = self.parent.query("#aptui").last()
+            new_req.query_one("#response_text", Static).update(curl_request)
+            new_req.scroll_visible()
+            # try:
+            parsed_curl = uncurl.parse_context(f"""{curl_request}""")
+            # except Exception as e:
+            url = parsed_curl.url
+            method = parsed_curl.method.upper()
+            parsed_headers = parsed_curl.headers.items()
+            data = parsed_curl.data
+            headers = {}
+            for k, v in parsed_headers:
+                headers[k] = v
+            new_req.query_one("#url").value = url
+            new_req.query_one("#body_inp").value = data or ""
+            self.method_choice = method
 
         elif button_id == "add_req":
             headers = RequestHeader(id="headers")
@@ -170,11 +224,12 @@ class RequestContainer(Static):
         """Create child widgets of a API Request."""
         yield Input(placeholder="URL", id="url")
         yield Button("Send", id="send", variant="success")
-        yield Button("Import", id="import", variant="error")
         yield RequestMethods("Rquest Methods", id="methods", classes="body")
         yield RequestHeader()
         yield Body(expand=True, id="body")
         yield Response("Response", expand=True, id="response")
+        yield Button("Copy Curl", id="tocurl")
+        yield Button("Import", id="importcurl", variant="error")
 
     def on_mount(self) -> None:
         self.styles.height = "auto"
